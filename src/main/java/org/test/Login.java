17717a5309690276c16dee3211e8b6c3168a09c6
Ramprabhu237 <124779131+Ramprabhu237@users.noreply.github.com>
2023-03-21 12:32:51 +0530

    Updated
    Updated code

--- src/main/java/org/test/Login.java
+++ src/main/java/org/test/Login.java
@@ -4,8 +4,9 @@ public class Login {
 public static void main(String[] args) {
 		
 		System.out.println("Hello Login");
-		System.out.println("Hi");
-		System.out.println("Welcome");
+		System.out.println("Hiii");
+		System.out.println("How are you");
+		
 
 	}
 }
